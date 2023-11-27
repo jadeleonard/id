eba7204f9b9a6c1082dc16e51a6edce869c78b02
jadeleonard <lukabartolomeo9@gmail.com>
2023-11-27 10:46:58 -0800

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+'use client'
+import React, { useState } from 'react'
+import { signIn } from 'next-auth/react'
 const App = () => {
+  const [userinfo,setUserinfo] = useState({email:'',password:''})
+const handleSubmit = async () => {
+  const res = await  signIn('credentials',{
+    email:userinfo.email,
+    password:userinfo.email,
+
+
+
+
+  });
+  console.log(res)
+}
   return (
     <div>
-        
+      <input onChange={({target}) => setUserinfo({...userinfo,email:target.value})}/>
+      <input onChange={({target}) => setUserinfo({...userinfo,password:target.value})}/>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   )
 }
